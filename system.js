03cf48b0f8858f1d8aa25ede66c0bf63a93e89c1
Lasse Damtoft Thoby <LasseThoby@Lasses-MacBook-Air-4.local>
2019-10-14 09:48:14 +0200

    heyhey

--- system.js
+++ system.js
@@ -1,1 +1,13 @@
-script to come
+class Reservation{
+    constructor(numGuests, date, time, comment){
+        this.numGuests = numGuests;
+        this.date = date;
+        this.time = time;
+        this.comment = comment;
+    }
+
+    Rersevation = Reservation(10, 10, 10, "hej");
+
+        console.log(Reservation.comment);
+    }
+}
