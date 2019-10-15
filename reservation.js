6244d1978638c1ceef2e3c6d516bc1960a59d25c
Lasse Damtoft Thoby <LasseThoby@Lasses-MacBook-Air-4.local>
2019-10-15 13:43:20 +0200

    nytnytnyt

--- reservation.js
+++ reservation.js
@@ -1,12 +1,46 @@
-class Reservation {
-    constructor(firstName, lastName, phoneNumber, email, numGuests, date, time, comment){
+class Reservation{
+    reservation(firstName, lastName, phoneNumber,){
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.numGuests = numGuests;
-        this.date = date;
-        this.time = time;
-        this.comment = comment;
-        }
+    }
+
+    reservation1 = new Reservation(
+        document.getElementById("firstName").value,
+        document.getElementById("lastName").value,
+        document.getElementById("phoneNumber").value,
+    );
+
+    localStorage.
+
+
+}
+
+for (const htmlElement of document = "frontpage.html") {
+
+}
+var firstName = document.getElementById("firstName").value;
+var lastName = document.getElementById("lastName").value;
+var phoneNumber = document.getElementById("phoneNumber").value;
+var email = document.getElementById("email");
+var numGuests = document.getElementById("numGuests");
+var date = document.getElementById("date");
+var time = document.getElementById("time");
+var comment = document.getElementById("comment");
+
+function storeLogin () {
+    localStorage.setItem('firstName');
+    localStorage.setItem('lastName');
+    localStorage.setItem('phoneNumber');
+    localStorage.setItem('email');
+    localStorage.setItem('numGuests');
+    localStorage.setItem('date');
+    localStorage.setItem('time');
+    localStorage.setItem('comment');
+    alert('Reservation has been made');
+
+}
+function showUser () {
+    console.log('Show user stored in localStorage');
+    console.log(localStorage);
 }
