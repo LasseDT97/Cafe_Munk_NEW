4dec7727bd85333be9983edbec8c4e7841de4356
Lasse Damtoft Thoby <LasseThoby@Lasses-MacBook-Air-4.local>
2019-10-18 12:57:47 +0200

    java add

--- database.js
+++ database.js
@@ -1,14 +1,35 @@
-    var kunde1 = {
+var objekt = new objekt ();
+    reservation.firstName = "Anton";
+    reservation.lastName = "Søndergaard";
+    reservation.phoneNumber = "20108317";
+    reservation.email = "Anton@test.dk";
+    reservation.date = "05/03/96";
+    reservation.time = "2";
+    reservation.comment = "Vi ser frem til lækker maaaat"
+
+var costumer =  newArray();
+    costumer[0] = "firstName";
+    costumer [1] = "lastName";
+
+var jsonMethod = JSON.stringify(objekt, costumer)
+
+
+
+
         firstName: "Anton", lastName: "Søndergaard", phoneNumber: "20108317", email: "Anton@test.dk", date: "05/03/2019", time: 2, comment: "Vi ser frem til god maaat"};
-    var kunde2 = {
-        firstName: "Victoria", lastName: "Patrick", phoneNumber: "45768932", email: "Victoria@test.dk", date: "05/03/2019", time: 2, comment: "Vi ser frem til god maaat"};
+    var objekt2 = {
+        firstName: "Victoria", lastName: "Patrick", phoneNumber: "45768932", email: "Victoria@test.dk", date: "05/03/2019", time: "2", comment: "Vi ser frem til god maaat"};
 
     var admin1 = {firstName: "Anton", lastName: "Søndergaard", phoneNumber: "20108317", email: "Anton@test.dk", date: "05/03/2019", time: 2, comment: "Vi ser frem til god maaat";
     var admin2 = {firstName: "Anton", lastName: "Søndergaard", phoneNumber: "20108317", email: "Anton@test.dk", date: "05/03/2019", time: 2, comment: "Vi ser frem til god maaat"
 
     {
-    var kundeJSON = JSON.stringify(kunde1, kunde2);
+    var kundeJSON = JSON.stringify(objekt1);
+
+
+    console.log(kundeJSON);
 }
+
 
 
 
--- reservation.js
+++ reservation.js
@@ -7,26 +7,40 @@ class Reservation {
         this.date = date;
         this.time = time;
         this.comment = comment;
-    }
+    } }
     {
-    Reservation1 = new Reservation(
+var id_generator = 0;
+
+function id_gen() {
+    id_generator ++;
+    return id_generator;
+}
+}
+function reserver(){
+    if checkInput() == (true){//Husk at lave check input funktion
+    id_gen();
+    
+    var Reservation = new Reservation(
         document.getElementById("firstName").value,
         document.getElementById("lastName").value,
         document.getElementById("phoneNumber").value,
         document.getElementById("email").value,
         document.getElementById("date").value,
-        document.getElementById("time").value,
+}       document.getElementById("time").value,
         document.getElementById("comment").value,
-    );
-}
-console.log(.value);
-}
 
-for (const htmlElement of document = "frontpage.html") {
+    Reservation.id = id_generator;
+    console.log(Reservation);
 
-}
+    xx.push(Reservation);
+    console.log(Reservation);
 
-function reservation () {
+    //Pusher vores reservationsobjekter som et array ind i local storage.
+    local.storage.setItem(xx), JSON.stringify(xx)
+    console.log(JSON.parse(local.storage.getItem(xx)))
+
+
+    function reservation () {
     localStorage.setItem('firstName');
     localStorage.setItem('lastName');
     localStorage.setItem('phoneNumber');
