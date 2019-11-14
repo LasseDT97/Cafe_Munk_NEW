b617180f4b583cdca1f42a7e5d9a76a58217a767
Lasse Damtoft Thoby <LasseThoby@Lasses-MacBook-Air-4.local>
2019-11-14 08:32:08 +0100

    gennembrud

--- adminRegistration.js
+++ adminRegistration.js
@@ -1,62 +1,117 @@
-//Creation of class and constructors
+//Creation of class and constructors.
 class AdminRegistration {
-    constructor (userName, password, passwordAgain) {
-        this.userName = userName;
-        this.password = password;
-        this.passwordAgain = passwordAgain;
+        constructor(userName, password, passwordAgain) {
+            //Assigning variables for use later on.
+            this.userName = userName;
+            this.password = password;
+            this.passwordAgain = passwordAgain;
+        }
     }
-}
+
+        let adminRegistrationList = [];
+
+        adminRegistrationList.push(new AdminRegistration("Lasse", "Lasse1234", "Lasse1234"));
+        adminRegistrationList.push(new AdminRegistration("Victoria", "Victoria1234", "Victoria1234"));
+        adminRegistrationList.push(new AdminRegistration("Anton", "Anton1234", "Anton1234"));
+
+        console.log(adminRegistrationList);
+
+        for (let i = 0; i < adminRegistrationList.length; i++) {
+         JSON.parse(localStorage.getItem("allAdmins"));
+            }
+/*
+  function createUser () {
+
+            console.log("Calls user_id_generator");
+            user_id_generator();
+  }
+         var userObj = new User(
+        document.getElementById("userName").value,
+        document.getElementById("password").value,
+         document.getElementById("passwordAgain").value,);
+
+        adminRegistrationList.push(userObj);
+        console.log(adminRegistrationList);
+        console.log ("Ny bruker lagt til");
+
+        localStorage.setItem("allAdmins", JSON.stringify(adminRegistrationList));
+        console.log(JSON.parse(localStorage.getItem("allAdmins")));
+*/
+        //Creating a function to validate entries before storing them.
 function validateRegistration() {
 
-    let userName = document.getElementById("userName".value.);
-    let password = document.getElementById("password".value.);
-    let passwordAgain = document.getElementById("passwordAgain".value.);
+    //Getting values entered in the HTML document by using getElementById method.
+    let newAdmin = new AdminRegistration (
+    document.getElementById("userName").value,
+    document.getElementById("password").value,
+    document.getElementById("passwordAgain").value);
+    
+    adminRegistrationList.push(newAdmin);
+    console.log(adminRegistrationList);
+    console.log("Ny admin oprettet");
 
+    localStorage.setItem("allAdmins", JSON.stringify(adminRegistrationList));
+    console.log(JSON.parse(localStorage.getItem("allAdmins")));}
+/*
+    //Creating a new variable which contains the entries from above
     let newAdmin = new AdminRegistration(userName, password, passwordAgain);
+        JSON.parse(localStorage.getItem("newUser"));
+        adminRegistrationList.push(newAdmin);
+    }
+    
 
-    /*let validation_message = "";
 
-    if (userName == null || userName === "") {
-        validation_message += "Du skal udfylde et brugernavn \n";
-        return false;
+    //reservationList.push(newAdmin); }
+    /*
+    //Creating a function that checks if the user is already to be found in the localStorage.
+    function checkExistingUser() {
+
+        //Collecting values from the localStorage.
+        let localUser = localStorage.getItem("userName").value;
+        let localPassword = localStorage.getItem("password").value;
+        let localPasswordAgain = localStorage.getItem("passwordAgain").value;
+
+        //If the entered username and password and passwordagain is already to be found in localStorage it should add
+        // to the validation message.
+        if (userName === localUser && password === localPassword && passwordAgain === localPasswordAgain) {
+            alert("Brugeren findes allerede");
+        }
+        if (userName == null || userName === "") {
+            validation_message += "Du skal udfylde et brugernavn \n";
+            return false;
+        }
+
+        if (userName.length <= 8) {
+            validation_message += "Brugernavnet skal have mindst 8 tegn \n";
+            return false;
+        }
+
+        if (password == null || password === "") {
+            validation_message += "Du skal udfylde et password \n";
+            return false;
+        }
+
+        if (password.length <= 8) {
+            validation_message += "Adgangskoden skal have mindst 8 tegn \n";
+            return false;
+        }
     }
 
-    if (userName.length <= 8) {
-        validation_message += "Brugernavnet skal have mindst 8 tegn \n";
-        return false;
-    }
 
-    if (password == null || password === "") {
-        validation_message += "Du skal udfylde et password \n";
-        return false;
-    }
-
-    if (password.length <= 8) {
-        validation_message += "Adgangskoden skal have mindst 8 tegn \n";
-        return false;
-    }
-
-    function checkExistingUser {
-               let xxx = localStorage.getItem("userName");
-               let yyy = localStorage.getItem("password");
-               let zzz = localStorage.getItem("passwordAgain");
-
-               let = enteredUser = document.getElementById("userName");
-               let = enteredPassword = document.getElementById("password");
-               let = enteredPasswordAgain = document.getElementById("passwordAgain");
-
-               if(enteredUser === xxx && enteredPassword === yyy && enteredPasswordAgain === zzz) {
-                   alert("din kode virker");
-               }
-
-    } */
 }
+function storeAdminRegistration() {
+    localStorage.setItem(newAdmin);
+}
+
+console.log(storeAdminRegistration);
+
+
 
 /*if (form_validation = true) {
     alert(validation_form);
 } */
 
-function storeAdminRegistration() {
+/*function storeAdminRegistration() {
     localStorage.setItem(new AdminRegistration();
 }
 console.log(newAdmin);
